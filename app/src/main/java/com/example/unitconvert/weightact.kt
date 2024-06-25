c3115403d6ac505ae67b8a32ccadb9920dd80cb9
Manmohan Shrivastava <kabirismanmohan@gmail.com>
2024-06-25 09:21:13 +0530

    weightworking

--- app/src/main/java/com/example/unitconvert/weightact.kt
+++ app/src/main/java/com/example/unitconvert/weightact.kt
@@ -1,20 +1,112 @@
 package com.example.unitconvert
 
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.unitconvert.databinding.ActivityMainBinding
+import com.example.unitconvert.databinding.ActivityWeightactBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import kotlin.math.roundToLong
 
 class weightact : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding: ActivityWeightactBinding by lazy {
+            ActivityWeightactBinding.inflate(layoutInflater)
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_weightact)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val weightunits = resources.getStringArray(R.array.weightunits)
+        val arrayadapter = ArrayAdapter(this, R.layout.dropdown_item, weightunits)
+        binding.fromunit.setAdapter(arrayadapter)
+        binding.tounit.setAdapter(arrayadapter)
+
+
+        val inttxt = binding.inttxt
+        val fromunit = binding.fromunit
+        val tounit = binding.tounit
+        val outtxt = binding.outtxt
+        val wunittxt = binding.wunittext
+        val wunittxt2 = binding.wunittext2
+
+
+        val convertbutton = binding.convertbutton
+        convertbutton.setOnClickListener {
+            val out = weightcal(
+                inttxt.text.toString().toInt(),
+                fromunit.text.toString(),
+                tounit.text.toString()
+            )
+            val gg = ("%.4f".format(out))
+            outtxt.text = ("$gg"+" ${tounit.text.toString()}")
+
+        }
+
+
     }
+
+    private fun weightcal(int: Int, s: String, s1: String): Double {
+        var a = 00.00
+        when(s){
+            "Kg"->{
+                when(s1){
+                    "Pounds"->{
+                        a = int * 2.20462
+                    }
+                    "g"->{
+                        a = int * 1000.0
+                    }
+                    "Kg"->{
+                        a = int+ 00.00
+                    }
+                }
+            }
+            "g"->{
+                when(s1){
+                    "Pounds"->{
+                        a = int * 0.00220462
+                    }
+                    "g"->{
+                        a = int+ 00.00
+                    }
+                    "Kg"->{
+                        a = int * 0.001
+                    }
+                }
+
+            }
+            "Pounds"->{
+                when(s1){
+                    "Pounds"->{
+                        a = int + 00.00
+                    }
+                    "g"->{
+                        a = int * 453.592
+                    }
+                    "Kg"->{
+                        a = int * 0.453592
+                    }
+                }
+
+            }
+        }
+        return a
+
+    }
+
+
 }
